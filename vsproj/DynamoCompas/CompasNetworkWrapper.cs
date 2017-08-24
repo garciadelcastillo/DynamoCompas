e13c20d12dacf1b0c441fccdea5a5677e20acce5
Jose Luis Garcia del Castillo <personal@garciadelcastillo.es>
2017-08-24 14:26:46 +0200

    Base wrapper

--- vsproj/DynamoCompas/CompasNetworkWrapper.cs
+++ vsproj/DynamoCompas/CompasNetworkWrapper.cs
@@ -3,35 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using Autodesk.DesignScript.Runtime;
 using Autodesk.DesignScript.Interfaces;
 using Autodesk.DesignScript.Geometry;
 
-namespace DynamoCompas
+namespace ZeroTouchEssentials
 {
-    public class DynamoCompas
-    {
-
-        /// <summary>
-        /// Placeholder for the creation of a network element
-        /// </summary>
-        /// <param name="mesh"></param>
-        /// <returns></returns>
-        public static CompasNetworkWrapper Create(Mesh mesh) => new CompasNetworkWrapper(mesh);
-
-        public static CompasNetworkWrapper Smooth(CompasNetworkWrapper network, int iteration)
-        {
-
-
-            return null;
-        }
-
-        
-
-
-
-    }
-
 
     [IsVisibleInDynamoLibrary(false)]
     public class CompasNetworkWrapper : IGraphicItem
