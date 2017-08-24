079f57b85c7750afde73dd88a67ad336008b5e63
Nono Martínez Alonso <mundowarezweb@gmail.com>
2017-08-24 14:39:54 +0200

    Add CompasNetwork class; Add NetworkFromObj ZT node

--- vsproj/DynamoCompas/DynamoCompas.cs
+++ vsproj/DynamoCompas/DynamoCompas.cs
@@ -24,6 +24,49 @@ namespace DynamoCompas
             
             return null;
         }
+
+        public static CompasNetwork CompasNetworkFromObj(string filePath = null)
+        {
+            var pySrc =
+@"
+import sys
+sys.path.append(r'C:\Program Files (x86)\IronPython 2.7\Lib')
+sys.path.append(r'C:\Users\martinno\repos\compas\src_load')
+import compas
+from compas.datastructures.network import Network
+# import List class to cast the type
+from System.Collections.Generic import *
+
+def NetworkFromObject(filepath):
+
+    # import network
+    network = Network.from_obj(filepath)
+
+    # extract network vertices
+    xyz = [network.vertex_coordinates(key) for key in network.vertices()]
+    vertices = List[object]([List[object]([x, y, z]) for x, y, z in xyz])
+
+    # extract network edges
+    key_index = network.key_index()
+    edges = [(key_index[u], key_index[v]) for u, v in network.edges()]
+    edges = List[object]([List[object](ij) for ij in edges])
+
+    return List[object]([network, str(network), vertices, edges])
+";
+            if (filePath != null || filePath != "")
+            {
+
+                // host python and execute script
+                var engine = IronPython.Hosting.Python.CreateEngine();
+                var scope = engine.CreateScope();
+                engine.Execute(pySrc, scope);
+
+                var NetworkFromObject = scope.GetVariable<Func<string, List<object>>>("NetworkFromObject");
+                var networkList = NetworkFromObject(filePath);
+                return CompasNetwork.Create(networkList[0], networkList[1] as String, networkList[2] as List<object>, networkList[3] as List<object>);
+            }
+            return null;
+        }
     }
 
 }
